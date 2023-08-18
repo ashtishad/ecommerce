c5e90842c4455511f65e1fcee568e20aa4343def
ashtishad <asheftishad@gmail.com>
2023-08-18 16:59:10 +0600

    Created writeResponse for write api endpoint response data and correct http status code in response. * added test for it

--- cmd/app/app_helpers.go
+++ cmd/app/app_helpers.go
@@ -1,8 +1,10 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -22,5 +24,14 @@ func sanityCheck() {
 		if os.Getenv(k) == "" {
 			log.Println(fmt.Sprintf("environment variable %s not defined. Terminating application...", k))
 		}
+	}
+}
+
+// writeResponse writes api endpoint response data and correct http status code in response.
+func writeResponse(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		panic(err)
 	}
 }
--- cmd/app/app_helpers_test.go
+++ cmd/app/app_helpers_test.go
@@ -3,6 +3,8 @@ package app
 import (
 	"bytes"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
@@ -24,5 +26,31 @@ func TestSanityCheck(t *testing.T) {
 	// Check the log output
 	if !bytes.Contains(buf.Bytes(), []byte("environment variable SERVER_ADDRESS not defined")) {
 		t.Error("Expected log message not found")
+	}
+}
+
+func TestWriteResponse(t *testing.T) {
+	tests := []struct {
+		code     int
+		data     interface{}
+		expected string
+	}{
+		{http.StatusOK, map[string]string{"message": "success"}, `{"message":"success"}`},
+		{http.StatusBadRequest, map[string]string{"error": "bad request"}, `{"error":"bad request"}`},
+		{http.StatusInternalServerError, map[string]string{"error": "internal server error"}, `{"error":"internal server error"}`},
+	}
+
+	for _, test := range tests {
+		rr := httptest.NewRecorder()
+		writeResponse(rr, test.code, test.data)
+
+		if status := rr.Code; status != test.code {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, test.code)
+		}
+
+		expected := test.expected + "\n"
+		if rr.Body.String() != expected {
+			t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+		}
 	}
 }
