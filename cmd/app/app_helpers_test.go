a836f4c7cc0f573209b5c965e017d474cce3cdae
ashtishad <asheftishad@gmail.com>
2023-08-18 17:42:19 +0600

    Ignored returning error of os.Unsetenv
    * removed unnecessary comments.

--- cmd/app/app_helpers_test.go
+++ cmd/app/app_helpers_test.go
@@ -11,20 +11,16 @@ import (
 )
 
 func TestSanityCheck(t *testing.T) {
-	// Capture log output
 	var buf bytes.Buffer
 	log.SetOutput(&buf)
 	defer func() {
 		log.SetOutput(os.Stderr)
 	}()
 
-	// Unset environment variable to trigger log message
-	os.Unsetenv("SERVER_ADDRESS")
+	_ = os.Unsetenv("SERVER_ADDRESS")
 
-	// Run the sanity check
 	sanityCheck()
 
-	// Check the log output
 	if !bytes.Contains(buf.Bytes(), []byte("environment variable SERVER_ADDRESS not defined")) {
 		t.Error("Expected log message not found")
 	}
