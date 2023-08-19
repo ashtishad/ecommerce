de602227d9f5d2c038d2ebd7cb8f9720f326f6e2
ashtishad <asheftishad@gmail.com>
2023-08-19 13:31:15 +0600

    Updated Save method of User repository db to: 1. create if not exists 2. update if exists

--- domain/user_repository_db.go
+++ domain/user_repository_db.go
@@ -17,28 +17,37 @@ func NewUserRepositoryDB(dbClient *sql.DB, l *log.Logger) UserRepositoryDB {
 	return UserRepositoryDB{dbClient, l}
 }
 
-// Save is responsible for creating user from fields provided in domain.NewUserRequestDTO
+// Save is responsible for creating user(if not exist) from fields provided in domain.NewUserRequestDTO
+// if user already exists in the database, then updates it,
 // return sql.ErrNoRows or internal server error if some error occurs in database side.
 // To ensure data integrity, it refetch user information with the help of findUserByID method.
 func (d UserRepositoryDB) Save(user User) (User, error) {
-	const sqlInsertUser = `
-	INSERT INTO users ( email, password_hash, full_name, phone, sign_up_option ) 
-	VALUES ( ?, ?, ?, ?, ?)`
+	existingUser, err := d.FindExisting(user.Email, user.PasswordHash)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return User{}, fmt.Errorf("error checking existing user: %v", err)
+	}
 
+	if err == nil {
+		// User exists, so update
+		_, err = d.db.ExecContext(context.Background(), sqlUpdateUser, user.PasswordHash, user.FullName, user.Phone, user.SignUpOption, user.Email)
+		if err != nil {
+			return User{}, fmt.Errorf("error updating user: %v", err)
+		}
+		return d.findUserByID(existingUser.UserID)
+	}
+
+	// User doesn't exist, so insert
 	result, err := d.db.ExecContext(context.Background(), sqlInsertUser, user.Email, user.PasswordHash, user.FullName, user.Phone, user.SignUpOption)
 	if err != nil {
-		//d.l.Println(err.Error())
 		return User{}, fmt.Errorf("error inserting user: %v", err)
 	}
 
 	id, err := result.LastInsertId()
 	if err != nil || id == 0 {
-		//d.l.Println(err.Error())
 		return User{}, fmt.Errorf("error getting last inserted user ID: %v", err)
 	}
 
-	user, err = d.findUserByID(int(id))
-	return user, err
+	return d.findUserByID(int(id))
 }
 
 // FindExisting takes user email and password hash string and returns existing user's record
@@ -63,8 +72,7 @@ func (d UserRepositoryDB) FindExisting(email string, pass string) (User, error) 
 // findUserByID takes userId and returns a single user's record
 // returns error if internal server error happened.
 func (d UserRepositoryDB) findUserByID(userID int) (User, error) {
-	query := `SELECT user_id, user_uuid, email, password_hash, full_name, phone, sign_up_option, status, created_at, updated_at FROM users WHERE user_id = ?`
-	row := d.db.QueryRow(query, userID)
+	row := d.db.QueryRow(sqlFindUserByID, userID)
 
 	var user User
 	err := row.Scan(&user.UserID, &user.UserUUID, &user.Email, &user.PasswordHash, &user.FullName, &user.Phone, &user.SignUpOption, &user.Status, &user.CreatedAt, &user.UpdatedAt)
---
+++ domain/user_sql_queries.go
@@ -0,0 +1,15 @@
+package domain
+
+const (
+	sqlInsertUser = `
+	INSERT INTO users ( email, password_hash, full_name, phone, sign_up_option ) 
+	VALUES ( ?, ?, ?, ?, ?)`
+
+	sqlUpdateUser = `
+		UPDATE users 
+		SET password_hash = ?, full_name = ?, phone = ?, sign_up_option = ?
+		WHERE email = ?`
+
+	sqlFindUserByID = `SELECT user_id, user_uuid, email, password_hash, full_name, phone, sign_up_option, status, created_at, updated_at 
+                       FROM users WHERE user_id = ?`
+)
