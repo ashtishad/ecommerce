c52d550196fc6169124340defb71af37e2a00e19
ashtishad <asheftishad@gmail.com>
2023-08-19 23:16:58 +0600

    Implemented core salt mechanism from db side and service. * on user create/update it will generate new salt. * on Find users by email and password, it takes the password from input, retrives stored salt value, makes hashed password and finally check the email and password matches or not, if matches then returns the result.

--- domain/user_sql_queries.go
+++ domain/user_sql_queries.go
@@ -13,5 +13,12 @@ const (
 	sqlFindUserByID = `SELECT user_id, user_uuid, email, password_hash, full_name, phone, sign_up_option, status, created_at, updated_at 
                        FROM users WHERE user_id = ?`
 
-	findExistingUserByEmail = `SELECT user_id, user_uuid, email, password_hash, full_name, phone, sign_up_option, status, created_at, updated_at FROM users WHERE email = ?`
+	sqlFindUserByEmail = `SELECT user_id, user_uuid, email, password_hash, full_name, phone, sign_up_option, status, created_at, updated_at 
+                       FROM users WHERE email = ?`
+	sqlFindUserIDFromEmail = `SELECT user_id FROM users WHERE email = ?`
+	sqlInsertUserIDSalt    = `INSERT INTO user_salts (user_id, salt) VALUES (?, ?) ON DUPLICATE KEY UPDATE salt = ?`
+	sqlFindSaltByMail      = `SELECT s.salt FROM user_salts s 
+                          JOIN users u ON s.user_id = u.user_id 
+                          WHERE u.email = ?`
+	sqlFindUserByMailAndPass = `SELECT * FROM users WHERE email = ? AND password_hash = ?`
 )
