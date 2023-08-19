4dc6204490e0b3670f01294ec9dbadc9b6778044
ashtishad <asheftishad@gmail.com>
2023-08-19 09:34:36 +0600

    Added service for exisitng user * logic is pretty much straightforward and generate data from domain

--- service/user_service.go
+++ service/user_service.go
@@ -6,6 +6,7 @@ import (
 
 type UserService interface {
 	NewUser(request domain.NewUserRequestDTO) (*domain.UserResponseDTO, error)
+	ExistingUser(request domain.ExistingUserRequestDTO) (*domain.UserResponseDTO, error)
 }
 
 type DefaultUserService struct {
@@ -43,6 +44,34 @@ func (service DefaultUserService) NewUser(request domain.NewUserRequestDTO) (*do
 		Status:       createdUser.Status,
 		CreatedAt:    createdUser.CreatedAt,
 		UpdatedAt:    createdUser.UpdatedAt,
+	}
+
+	return userResponseDTO, nil
+}
+
+// ExistingUser first converts Convert request DTO to a user domain model (just email and password is filled)
+// then Calls the repository to save the new user, get the user model if everything okay, otherwise returns error
+// Finally converts to UserResponseDTO.
+func (service DefaultUserService) ExistingUser(request domain.ExistingUserRequestDTO) (*domain.UserResponseDTO, error) {
+	userRequest := domain.User{
+		Email:        request.Email,
+		PasswordHash: request.Password,
+	}
+
+	existingUser, err := service.repo.FindExisting(userRequest.Email, userRequest.PasswordHash)
+	if err != nil {
+		return nil, err
+	}
+
+	userResponseDTO := &domain.UserResponseDTO{
+		UserUUID:     existingUser.UserUUID,
+		Email:        existingUser.Email,
+		FullName:     existingUser.FullName,
+		Phone:        existingUser.Phone,
+		SignUpOption: existingUser.SignUpOption,
+		Status:       existingUser.Status,
+		CreatedAt:    existingUser.CreatedAt,
+		UpdatedAt:    existingUser.UpdatedAt,
 	}
 
 	return userResponseDTO, nil
