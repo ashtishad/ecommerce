78b53f9d2ae842a4749f9240c4f16a7ca39109b0
ashtishad <asheftishad@gmail.com>
2023-08-23 11:47:34 +0600

    Removed outdated secrets keys

--- start.sh
+++ start.sh
@@ -6,7 +6,5 @@ export DB_PASSWD=root
 export DB_ADDR=localhost
 export DB_PORT=3306
 export DB_NAME=users
-export GOOGLE_AUTH_CLIENT_ID=594665614368-76ld992rnjnlj74o79lalajb3c3u5dff.apps.googleusercontent.com
-export GOOGLE_AUTH_CLIENT_SECRET=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
 export GOOGLE_AUTH_REDIRECT_URL=http://localhost:8000/google-auth/callback
 go run main.go
