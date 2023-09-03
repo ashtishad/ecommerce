d1b33bc003065b75d10c080d6c5b1139b94283d3
ashtishad <ashefgo@gmail.com>
2023-09-04 01:24:24 +0600

    Fixed linting warning: type assertion must be checked (forcetypeassert)

--- users-api/internal/service/mock_user_service.go
+++ users-api/internal/service/mock_user_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"github.com/ashtishad/ecommerce/lib"
 	"github.com/ashtishad/ecommerce/users-api/internal/domain"
 	"github.com/stretchr/testify/mock"
@@ -16,9 +17,17 @@ func (m *MockUserService) NewUser(request domain.NewUserRequestDTO) (*domain.Use
 		return nil, nil
 	}
 	if args.Get(0) != nil {
-		return args.Get(0).(*domain.UserResponseDTO), nil
+		userResponse, ok := args.Get(0).(*domain.UserResponseDTO)
+		if !ok {
+			return nil, lib.NewInternalServerError("failed type assertion", errors.New("internal error"))
+		}
+		return userResponse, nil
 	}
-	return nil, args.Get(1).(lib.APIError)
+	apiError, ok := args.Get(1).(lib.APIError)
+	if !ok {
+		return nil, lib.NewInternalServerError("failed type assertion", errors.New("internal error"))
+	}
+	return nil, apiError
 }
 
 func (m *MockUserService) UpdateUser(request domain.UpdateUserRequestDTO) (*domain.UserResponseDTO, lib.APIError) {
@@ -27,9 +36,17 @@ func (m *MockUserService) UpdateUser(request domain.UpdateUserRequestDTO) (*doma
 		return nil, nil
 	}
 	if args.Get(0) != nil {
-		return args.Get(0).(*domain.UserResponseDTO), nil
+		userResponse, ok := args.Get(0).(*domain.UserResponseDTO)
+		if !ok {
+			return nil, lib.NewInternalServerError("failed type assertion", errors.New("internal error"))
+		}
+		return userResponse, nil
 	}
-	return nil, args.Get(1).(lib.APIError)
+	apiError, ok := args.Get(1).(lib.APIError)
+	if !ok {
+		return nil, lib.NewInternalServerError("failed type assertion ", errors.New("internal error"))
+	}
+	return nil, apiError
 }
 
 func (m *MockUserService) GetAllUsers(request domain.FindAllUsersOptionsDTO) ([]domain.UserResponseDTO, *domain.NextPageInfo, lib.APIError) {
@@ -38,7 +55,16 @@ func (m *MockUserService) GetAllUsers(request domain.FindAllUsersOptionsDTO) ([]
 		return nil, nil, nil
 	}
 	if args.Get(0) != nil && args.Get(1) != nil {
-		return args.Get(0).([]domain.UserResponseDTO), args.Get(1).(*domain.NextPageInfo), nil
+		userResponseSlice, ok1 := args.Get(0).([]domain.UserResponseDTO)
+		nextPageInfo, ok2 := args.Get(1).(*domain.NextPageInfo)
+		if !ok1 || !ok2 {
+			return nil, nil, lib.NewInternalServerError("failed type assertion", errors.New("internal error"))
+		}
+		return userResponseSlice, nextPageInfo, nil
 	}
-	return nil, nil, args.Get(2).(lib.APIError)
+	apiError, ok := args.Get(2).(lib.APIError)
+	if !ok {
+		return nil, nil, lib.NewInternalServerError("failed type assertion", errors.New("internal error"))
+	}
+	return nil, nil, apiError
 }
