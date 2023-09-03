1da64641ed6f5ee40491efa24b54ee6027b1f0eb
ashtishad <ashefgo@gmail.com>
2023-09-04 00:29:51 +0600

    Added test for update user handler.

--- users-api/cmd/app/handlers_test.go
+++ users-api/cmd/app/handlers_test.go
@@ -22,8 +22,8 @@ func TestCreateUserHandler(t *testing.T) {
 
 	expectedUserResponse := &domain.UserResponseDTO{
 		UserUUID:     "some-uuid",
-		Email:        "keanu_reeves@gmail.com",
-		FullName:     "Keanu Reeves",
+		Email:        "john_wick@gmail.com",
+		FullName:     "John Wick",
 		Phone:        "1234567890",
 		SignUpOption: "general",
 		Status:       "active",
@@ -35,9 +35,9 @@ func TestCreateUserHandler(t *testing.T) {
 	us := UserHandlers{service: mockUserService}
 
 	newUserRequest := domain.NewUserRequestDTO{
-		Email:        "KeeanuReeves@outlook.com",
-		FullName:     "Keanu Reeves",
-		Password:     "secrpsswrd",
+		Email:        "john_wick@gmail.com",
+		FullName:     "John Wick",
+		Password:     "secret_pass",
 		Phone:        "1234567890",
 		SignUpOption: "general",
 		Timezone:     "Asia/Dhaka",
@@ -48,6 +48,54 @@ func TestCreateUserHandler(t *testing.T) {
 	r.POST("/users", us.createUserHandler)
 
 	req, _ := http.NewRequest("POST", "/users", bytes.NewBuffer(data))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusOK, resp.Code)
+
+	var receivedUser domain.UserResponseDTO
+	err := json.NewDecoder(resp.Body).Decode(&receivedUser)
+	assert.Nil(t, err)
+
+	assert.Equal(t, expectedUserResponse, &receivedUser)
+}
+
+func TestUpdateUserHandler(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	r := gin.Default()
+
+	mockUserService := new(service.MockUserService)
+
+	expectedUserResponse := &domain.UserResponseDTO{
+		UserUUID:     "some-uuid",
+		Email:        "keanu_reeves@gmail.com",
+		FullName:     "Keanu Reeves",
+		Phone:        "9876543210",
+		SignUpOption: "general",
+		Status:       "active",
+		Timezone:     "Asia/Dhaka",
+	}
+
+	updateUserRequest := domain.UpdateUserRequestDTO{
+		UserUUID: "some-uuid",
+		Email:    "keanu_reeves@gmail.com",
+		FullName: "Keanu Reeves",
+		Phone:    "9876543210",
+		Timezone: "Asia/Dhaka",
+	}
+
+	mockUserService.On("UpdateUser", mock.AnythingOfType("domain.UpdateUserRequestDTO")).Return(expectedUserResponse, nil)
+
+	us := UserHandlers{service: mockUserService}
+
+	data, _ := json.Marshal(updateUserRequest)
+
+	r.PUT("/users/:user_id", us.updateUserHandler)
+
+	req, _ := http.NewRequest("PUT", "/users/some-uuid", bytes.NewBuffer(data))
 	req.Header.Set("Content-Type", "application/json")
 
 	resp := httptest.NewRecorder()
