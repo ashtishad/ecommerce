775eede5591028b75d5fe5ec9ade41d2ade300ed
ashtishad <ashefgo@gmail.com>
2023-09-13 12:08:10 +0600

    Modified domain structs for getting category with nested sub-categories

--- product-api/internal/domain/category_dto.go
+++ product-api/internal/domain/category_dto.go
@@ -3,19 +3,15 @@ package domain
 import "time"
 
 type CategoryResponseDTO struct {
-	CategoryUUID     string             `json:"categoryUuid"`
-	Name             string             `json:"name"`
-	Description      string             `json:"description"`
-	Status           string             `json:"status"`
-	CreatedAt        time.Time          `json:"createdAt"`
-	UpdatedAt        time.Time          `json:"updatedAt"`
-	HasSubcategories bool               `json:"hasSubcategories"`
-	SubCategories    []SubCategoryBrief `json:"subCategories"`
-}
-
-type SubCategoryBrief struct {
-	UUID string `json:"uuid"`
-	Name string `json:"name"`
+	CategoryUUID       string                `json:"categoryUuid"`
+	ParentCategoryUUID string                `json:"parentCategoryUuid"`
+	Name               string                `json:"name"`
+	Description        string                `json:"description"`
+	Status             string                `json:"status"`
+	CreatedAt          time.Time             `json:"createdAt"`
+	UpdatedAt          time.Time             `json:"updatedAt"`
+	Level              int                   `json:"level"`
+	Subcategories      []CategoryResponseDTO `json:"subcategories"`
 }
 
 type NewCategoryRequestDTO struct {
