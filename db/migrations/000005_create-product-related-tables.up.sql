e7a5494ffa554016db875efb8eb3d4c95220eab9
ashtishad <ashefgo@gmail.com>
2023-09-18 12:54:16 +0600

    Added domain models for product
    * Storing price in cents across all records.

--- db/migrations/000005_create-product-related-tables.up.sql
+++ db/migrations/000005_create-product-related-tables.up.sql
@@ -1,14 +1,14 @@
 BEGIN;
 
-CREATE TYPE currency_type AS ENUM ('USD', 'Taka', 'INR');
 CREATE TYPE sim_type AS ENUM ('Single', 'Dual');
 CREATE TYPE codec_type AS ENUM ('SBC', 'AAC', 'Aptx', 'LDAC');
 CREATE TYPE strap_type AS ENUM ('Steel', 'Metal', 'Leather');
+CREATE TYPE variant_type AS ENUM ('UAE', 'Official', 'USA','China', 'UK');
 
 CREATE TABLE IF NOT EXISTS products
 (
     product_id       SERIAL PRIMARY KEY,
-    product_uuid     uuid         NOT NULL DEFAULT uuid_generate_v4(),
+    product_uuid uuid NOT NULL DEFAULT uuid_generate_v4(),
     root_category_id INT REFERENCES categories (category_id),
     sku              VARCHAR(255) NOT NULL UNIQUE,
     created_at       TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -18,35 +18,31 @@ CREATE TABLE IF NOT EXISTS products
 CREATE TABLE IF NOT EXISTS phone_attributes
 (
     product_id     INT REFERENCES products (product_id),
-    price          numeric       NOT NULL,
-    currency       currency_type NOT NULL,
+    price_in_cents BIGINT CHECK (price_in_cents >= 0),
     sim_type       sim_type      NOT NULL,
-    storage        VARCHAR(255),
-    variant        VARCHAR(255),
-    stock          INT,
-    specifications JSONB,
+    storage VARCHAR(255) NOT NULL,
+    variant variant_type NOT NULL default 'Official',
+    stock   INT          NOT NULL default 0,
     PRIMARY KEY (product_id)
 );
 
 CREATE TABLE IF NOT EXISTS sound_equipment_attributes
 (
     product_id     INT REFERENCES products (product_id),
-    price          numeric       NOT NULL,
-    currency       currency_type NOT NULL,
+    price_in_cents BIGINT CHECK (price_in_cents >= 0),
     codecs         codec_type[]  NOT NULL,
-    color          VARCHAR(255),
-    specifications JSONB,
+    color VARCHAR(255) NOT NULL,
+    stock INT          NOT NULL default 0,
     PRIMARY KEY (product_id)
 );
 
 CREATE TABLE IF NOT EXISTS wearables_attributes
 (
     product_id     INT REFERENCES products (product_id),
-    price          numeric       NOT NULL,
-    currency       currency_type NOT NULL,
+    price_in_cents BIGINT CHECK (price_in_cents >= 0),
     strap_types    strap_type[]  NOT NULL,
-    color          VARCHAR(255),
-    specifications JSONB,
+    color VARCHAR(255) NOT NULL,
+    stock INT          NOT NULL default 0,
     PRIMARY KEY (product_id)
 );
 
---
+++ product-api/internal/domain/product.go
@@ -0,0 +1,73 @@
+package domain
+
+import (
+	"time"
+)
+
+type Product struct {
+	ProductID      int
+	ProductUUID    string
+	RootCategoryID int
+	SKU            string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+type PhoneAttribute struct {
+	ProductID    int
+	PriceInCents uint64
+	SimType      SimType
+	Storage      string
+	Variant      VariantType
+	Stock        int
+}
+
+type SoundEquipmentAttribute struct {
+	ProductID    int
+	PriceInCents uint64
+	Codecs       []CodecType
+	Color        string
+	Stock        int
+}
+
+type WearablesAttribute struct {
+	ProductID    int
+	PriceInCents uint64
+	StrapTypes   []StrapType
+	Color        string
+	Stock        int
+}
+
+type SimType string
+
+const (
+	Single SimType = "Single"
+	Dual   SimType = "Dual"
+)
+
+type CodecType string
+
+const (
+	SBC  CodecType = "SBC"
+	AAC  CodecType = "AAC"
+	Aptx CodecType = "Aptx"
+	LDAC CodecType = "LDAC"
+)
+
+type StrapType string
+
+const (
+	Steel   StrapType = "Steel"
+	Metal   StrapType = "Metal"
+	Leather StrapType = "Leather"
+)
+
+type VariantType string
+
+const (
+	UAE      VariantType = "UAE"
+	Official VariantType = "Official"
+	USA      VariantType = "USA"
+	China    VariantType = "China"
+	UK       VariantType = "UK"
+)
